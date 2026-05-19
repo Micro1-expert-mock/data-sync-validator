da7a923e034f4253d32518bf2df7f28ca65f9f1c
RomJusta <91089071+RomJusta@users.noreply.github.com>
2026-05-19 12:42:43 -0300

    Fix calculator growth logic and lint failure

--- src/calculator.py
+++ src/calculator.py
@@ -1,6 +1,2 @@
-import os  # LINT ERROR: Unused import (will fail flake8)
-import sys
-
 def calculate_growth(initial, current):
-    # LOGIC ERROR: Subtracting instead of calculating percentage growth
-    return (current - initial) - initial
+    return ((current - initial) / initial) * 100

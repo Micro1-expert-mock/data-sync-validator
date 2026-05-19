77a3efe52491d990e97703b9944e8282e0f64390
RomJusta <91089071+RomJusta@users.noreply.github.com>
2026-05-19 12:31:50 -0300

    Create calculator.py

---
+++ src/calculator.py
@@ -0,0 +1,6 @@
+import os  # LINT ERROR: Unused import (will fail flake8)
+import sys
+
+def calculate_growth(initial, current):
+    # LOGIC ERROR: Subtracting instead of calculating percentage growth
+    return (current - initial) - initial
